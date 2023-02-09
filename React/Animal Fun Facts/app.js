024018c87dceeda465fcdbcb424e5622261d80fa
Dkv3 <103575218+Dkv3@users.noreply.github.com>
2023-02-09 04:50:00 -0500

    Reorganized files and added an Authorization Form (#2)

--- React/Animal Fun Facts/app.js
+++ React/Animal Fun Facts/app.js
@@ -8,6 +8,8 @@ const background = (
   <img class="background" alt="ocean" src={"/images/ocean.jpg"} />
 );
 
+const showBackground = true;
+
 const images = [];
 for (const animal in animals) {
   images.push(
@@ -27,26 +29,23 @@ console.log(animals.dolphin.facts[0]);
 
 function displayFact(e) {
   const selectedAnimal = e.target.alt;
-  const index = Math.floor(Math.random() * 1);
-  const funFact = 'animals.selectedAnimal.facts';
+  const animalInObject = animals[selectedAnimal];
+  const index = Math.floor(Math.random() * animalInObject.facts.length);
+  const funFact = animalInObject.facts[index];
   document.getElementById('fact').innerHTML = funFact;
-  console.log(`funFact: ${funFact}`);
-  console.log(`index: ${index}`);
-  console.log(`selectedAnimal: ${selectedAnimal}`);
-  console.log(`animals.selectedAnimal:`, animals.selectedAnimal);
-  console.log('Dolphin fact:', animals.dolphin.facts[0]);
 }
-
-// (line 36): animals.selectedAnimal isn't real; need to find a way to use the actual variable, selectedAnimal, instead of the literal property name that isn't actually there
 
 const animalFacts = (
   <div>
     <h1>{title}</h1>
-    {title === "" ? "Click an animal for a fun fact!" : title}
-    {background}
+    {/*  {title === "" ? "Click an animal for a fun fact!" : title}*/}
+    {title || "Click an animal for a fun fact!"}
+    {showBackground && background}
     <div className="animals">{images}</div>
     <p id='fact'></p>
   </div>
 );
 
 ReactDOM.render(animalFacts, document.getElementById("root"));
+
+//     {title === "" ? "Click an animal for a fun fact!" : title}
