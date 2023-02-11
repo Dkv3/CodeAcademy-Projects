0dc9f324c2b7b67db23578ab98c2f15e155a89b0
1137072 <CThompmson4163@gmail.com>
2023-02-11 07:57:06 -0500

    Added react to addtional comp

--- React/myfirstreactapp/src/App.js
+++ React/myfirstreactapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Big Floppa
+          Big floppa
         </p>
         <a
           className="App-link"
