3a20335a32db475c1bc690830e74e302e1015e26
1137072 <CThompmson4163@gmail.com>
2023-03-17 08:38:57 -0400

    Further modified Powershell script to run as an import

--- Powershell/Get-SchoolNumber.ps1
+++ Powershell/Get-SchoolNumber.ps1
@@ -4,67 +4,101 @@ Get-SchoolNumber retrieves a school name from a provided school number.
 .DESCRIPTION
 Get-SchoolNumber checks for a valid 4 digit number and returns the AD name of the result.
 If the number entered does not exist, a prompt will be displayed to ask for another.
+If the -SchoolNumber paramater is provided with a four digit number, the command skips all prompts.
+If the -SchoolNumber parameter is provided with anything else, it will keep prompting for a four
+digit number, regardless of whether it is valid.
 .PARAMETER -SchoolNumber
 The school number that should be used. Using this param skips all prompts.
+.INPUTS
+A four digit school number provided by the user.
+.OUTPUTS
+This command returns the following cmdlet:
+Get-ADOrganizationalUnit -filter "Name -like '*$SchoolNumber'" | Select -ExpandProperty "Name",
+$SchoolNumber is supplied by the user, either through the -SchoolNumber param or the prompt if 
+that's not provided.
 .EXAMPLE
-Get-SchoolNumber -schoolnumber 2731
+Get-SchoolNumber
+Get-SchoolNumber -SchoolNumber 2731
 .NOTES
 Name: Get-SchoolNumber.ps1
 Author: Connor Thompson
-Last Modified: 3/16/2023 08:37
+Last Modified: 3/17/2023 05:57
 #>
 # Using "Active" and "Inactive" since true/false don't explicitly exist
-# To do:
-# - Add a param option to skip prompts that accepts a school number
-# - Add an if statement to filter by the 9000+ numbers
 
-$operation = "Active"
-do{
-$input = Read-Host -Prompt 'Enter a four digit school number'
-if ($input -match '\b\d{4}\b')
+Function Get-SchoolNumber
 {
-	$output = Get-ADOrganizationalUnit -filter "Name -like '*$input'" | Select -ExpandProperty "Name"
-	if ($output -eq $null)
+Param(
+[parameter(Mandatory = $false)]
+[int]$SchoolNumber = 0000
+)
+
+# Check to see if a param was supplied; doing so skips all of the prompts
+if ($SchoolNumber -ne 0000)
+{
+	$operation = "Inactive"
+	if ($SchoolNumber -match '\b\d{4}\b')
 	{
-		$response = "Inactive"
-		Write-Output "That school number does not exist. Please enter another."
+		Get-ADOrganizationalUnit -filter "Name -like '*$SchoolNumber'" | Select -ExpandProperty "Name"
 	}
 	else
 	{
-		Write-Output $output
-		$response = "Active"
-	}
-
-	if ($response -eq "Active")
-	{
-		do{
-		$response = Read-Host -Prompt "Enter another number? (Y / N)"
-		if ($response -eq "Y")
-		{
-			$operation = "Active"
-			$response = "Inactive"
-		}
-		elseif ($response -eq "N")
-		{
-			$operation = "Inactive"
-			$response = "Inactive"
-		}
-		else
-		{
-			Write-Output "You must enter either Y or N."
-			$operation = "Active"
-			$response = "Active"
-		}
-		}
-		While($response -eq "Active")
+		Write-Error -message 'A proper four digit school number was not supplied.'
 	}
 }
 else
 {
-	Write-Output "You must enter a 4 digit number!"
-}
-}
-While($operation -eq "Active")
+	$operation = "Active"
+	do
+	{
+		$input = Read-Host -Prompt 'Enter a four digit school number'
+		if ($input -match '\b\d{4}\b')
+		{
+			$output = Get-ADOrganizationalUnit -filter "Name -like '*$input'" | Select -ExpandProperty "Name"
+			if ($output -eq $null)
+			{
+				$response = "Inactive"
+				Write-Output "That school number does not exist. Please enter another."
+			}
+			else
+			{
+				Write-Output $output
+				$response = "Active"
+			}
 
+			if ($response -eq "Active")
+			{
+				do
+				{
+					$response = Read-Host -Prompt "Enter another number? (Y / N)"
+					if ($response -eq "Y")
+					{
+						$operation = "Active"
+						$response = "Inactive"
+					}
+					elseif ($response -eq "N")
+					{
+						$operation = "Inactive"
+						$response = "Inactive"
+					}
+					else
+					{
+						Write-Output "You must enter either Y or N."
+						$operation = "Active"
+						$response = "Active"
+					}
+				}
+				While($response -eq "Active")
+			}
+	}
+	else
+	{
+		Write-Output "You must enter a 4 digit number!"
+	}
+	}
+	While($operation -eq "Active")
+}
+}
 $VerbosePreference = "silentlyContinue"
 $DebugPreference = "silentlyContinue"
+Export-ModuleMember -Function 'Get-SchoolNumber'
